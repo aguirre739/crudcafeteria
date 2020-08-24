68e3487121212c4c547482ac672ae4ded1262abe
aguirre739 <pame11_@live.com>
2020-08-24 19:26:16 -0300

    se agregaron las rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
 import './bootstrap.min.css';
+import Inicio from './components/principal/Inicio';
+import ListarProductos from './components/productos/ListarProductos';
+import AgregarProducto from './components/productos/AgregarProducto';
+import EditarProducto from './components/productos/EditarProducto';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          <Inicio></Inicio>
+        </Route>
+        <Route exact path="/productos">
+        <ListarProductos></ListarProductos>
+        </Route>
+        <Route exact path="/productos/nuevo">
+          <AgregarProducto></AgregarProducto>
+        </Route>
+        <Route exact path="/productos/editar">
+          <EditarProducto></EditarProducto>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
   );
 }
 
