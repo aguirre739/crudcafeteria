8e0b721813e497616df14d5354269523d60519cf
aguirre739 <pame11_@live.com>
2020-08-26 16:47:54 -0300

    se agrego archivo json, navbar y componente de formulario

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {NavLink} from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
-            <h1>Header</h1>
+            <Navbar bg="primary" variant="dark" expand="lg">
+                <Navbar.Brand href="#home">Cafe Paris</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavLink exact={true} to="/" className="nav-link">Inicio</NavLink>
+                        <NavLink exact={true} to="/productos" className="nav-link">Productos</NavLink>
+                        <NavLink exact={true} to="/productos/nuevo" className="nav-link">Agregar Productos</NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     );
 };
--- src/components/productos/AgregarProducto.js
+++ src/components/productos/AgregarProducto.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 const AgregarProducto = () => {
-    return (
-        <div>
-            <h2>
-                pagina agregar producto
-            </h2>
-        </div>
-    );
+  return (
+    <Container className="d-flex flex-column justify-content-center align-items-center">
+      <h1 className="display-4 text-center my-4">Agregar Nuevo Producto</h1>
+      <Form className="w-75 shadow p-3 mb-5 bg-white rounded">
+        <Form.Group>
+          <Form.Label>Nombre del producto *</Form.Label>
+          <Form.Control type="text" placeholder="Ej. Cafe con leche" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label>Precio *</Form.Label>
+          <Form.Control type="text" placeholder="Ej. $50" />
+        </Form.Group>
+
+        <h3 className="text-center my-2">Categorias</h3>
+        <Form.Group className="my-3 text-center">
+          <div className="mb-3">
+            <Form.Check  type="radio" inline label="Bebida Caliente"/>
+            <Form.Check  type="radio" inline label="Bebida Fria"/>
+            <Form.Check  type="radio" inline label="Sandwich"/>
+            <Form.Check  type="radio" inline label="Dulce"/>
+            <Form.Check  type="radio" inline label="Salado"/>
+          </div>
+        </Form.Group>
+        <Button variant="primary" size="sm" block type="submit">
+          Agregar Producto
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default AgregarProducto;
